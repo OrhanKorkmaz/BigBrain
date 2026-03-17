6f8dff116185431885964a185a406e497acba660
OrhanKorkmaz <orhankorkmaz1991@gmail.com>
2026-03-17 22:59:11 +0300

    Create HistoryScreen.js

---
+++ bigbrainai/Screens/HistoryScreen.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HistoryScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>History</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#050510', alignItems: 'center', justifyContent: 'center' },
+  text: { color: '#fff', fontSize: 24 },
+});
